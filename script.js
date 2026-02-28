033b46a6a9e271dfd37d8c493f9ffdb34b5073f7
vsergiors <vsergiobusiness@gmail.com>
2026-02-28 23:41:21 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const SERVER_IP = "play.minewater.es";
+
+function copiarIP(){
+    navigator.clipboard.writeText(SERVER_IP);
+    alert("IP copiada");
+}
+
+async function updateServer(){
+    try{
+        const res = await fetch(`https://api.mcsrvstat.us/3/${minewater.noctier.net}`);
+        const data = await res.json();
+
+        if(data.online){
+
+            document.getElementById("status").innerText = "Online";
+            document.getElementById("mini-status").innerText = "Online";
+
+            document.getElementById("players").innerText = data.players.online;
+            document.getElementById("mini-players").innerText = data.players.online;
+
+            document.getElementById("max").innerText = data.players.max;
+            document.getElementById("version").innerText = data.version;
+
+            const list = document.getElementById("player-list");
+            list.innerHTML = "";
+
+            if(data.players.list){
+                data.players.list.forEach(p=>{
+                    const li = document.createElement("li");
+                    li.innerText = p;
+                    list.appendChild(li);
+                });
+            }
+
+        }else{
+            document.getElementById("status").innerText = "Offline";
+            document.getElementById("mini-status").innerText = "Offline";
+        }
+
+    }catch(e){
+        console.error(e);
+    }
+}
+
+updateServer();
+setInterval(updateServer,15000);
